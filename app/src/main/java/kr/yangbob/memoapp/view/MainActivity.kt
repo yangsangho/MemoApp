a7a5e16413f70f1c2f861c55da4e5ded99a37aeb
yangsangho <yangsangho22@gmail.com>
2020-02-16 21:55:11 +0900

    Change MainLayout (from CoordinatorLayout to MotionLayout)

--- app/src/main/java/kr/yangbob/memoapp/view/MainActivity.kt
+++ app/src/main/java/kr/yangbob/memoapp/view/MainActivity.kt
@@ -35,7 +35,7 @@ class MainActivity : AppCompatActivity() {
             spanCount = 3
         } else {
             spanCount = 2
-            setScrollEvent()
+//            setScrollEvent()
         }
         val memoAdapter = MemoListAdapter()
 
@@ -57,42 +57,42 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    private fun setScrollEvent() {
-        val titleTextView = toolbar.javaClass.getDeclaredField("mTitleTextView").let {
-            it.isAccessible = true
-            it.get(toolbar) as TextView
-        }
-
-        val contentLayoutParams: ViewGroup.LayoutParams = contentLayout.layoutParams
-        val appHeight: Int = Point().let {
-            windowManager.defaultDisplay.getSize(it)
-            it.y
-        }
-        var scrollCriteria = 0f
-        var doubleScrollCriteria = 0f
-
-        appBar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
-            if (scrollCriteria == 0f) {
-                scrollCriteria = appBarLayout.totalScrollRange / 4f
-                doubleScrollCriteria = scrollCriteria * 2
-            }
-
-            contentLayoutParams.height = appHeight - (appBarLayout.height + verticalOffset)
-            contentLayout.layoutParams = contentLayoutParams
-
-            val offsetAbs = verticalOffset.absoluteValue
-
-            var titleAlpha =
-                    if (offsetAbs < scrollCriteria) 0f
-                    else (offsetAbs - scrollCriteria) / doubleScrollCriteria
-            if (titleAlpha > 1f) titleAlpha = 1f
-
-            val startTitleAlpha =
-                    if (offsetAbs > doubleScrollCriteria) 0f
-                    else 1f - offsetAbs / doubleScrollCriteria
-
-            titleTextView.alpha = titleAlpha
-            startTitleLayout.alpha = startTitleAlpha
-        })
-    }
+//    private fun setScrollEvent() {
+//        val titleTextView = toolbar.javaClass.getDeclaredField("mTitleTextView").let {
+//            it.isAccessible = true
+//            it.get(toolbar) as TextView
+//        }
+//
+//        val contentLayoutParams: ViewGroup.LayoutParams = contentLayout.layoutParams
+//        val appHeight: Int = Point().let {
+//            windowManager.defaultDisplay.getSize(it)
+//            it.y
+//        }
+//        var scrollCriteria = 0f
+//        var doubleScrollCriteria = 0f
+//
+//        appBar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
+//            if (scrollCriteria == 0f) {
+//                scrollCriteria = appBarLayout.totalScrollRange / 4f
+//                doubleScrollCriteria = scrollCriteria * 2
+//            }
+//
+//            contentLayoutParams.height = appHeight - (appBarLayout.height + verticalOffset)
+//            contentLayout.layoutParams = contentLayoutParams
+//
+//            val offsetAbs = verticalOffset.absoluteValue
+//
+//            var titleAlpha =
+//                    if (offsetAbs < scrollCriteria) 0f
+//                    else (offsetAbs - scrollCriteria) / doubleScrollCriteria
+//            if (titleAlpha > 1f) titleAlpha = 1f
+//
+//            val startTitleAlpha =
+//                    if (offsetAbs > doubleScrollCriteria) 0f
+//                    else 1f - offsetAbs / doubleScrollCriteria
+//
+//            titleTextView.alpha = titleAlpha
+//            startTitleLayout.alpha = startTitleAlpha
+//        })
+//    }
 }
